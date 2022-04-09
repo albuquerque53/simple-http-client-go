8e81d4d21124922de548f0892bd91cbfa17a2db2
albuquerque53 <gabrielalbuquerque-dev@hotmail.com>
2022-04-09 11:07:46 -0300

    feat: receiving arguments from command line

--- app/main.go
+++ app/main.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"simplehttpclient/app/service"
 )
 
 func main() {
-	response := service.MakeRequest("GET", "https://duck.com")
+	httpMethod := os.Args[1]
+	target := os.Args[2]
+
+	response := service.MakeRequest(httpMethod, target)
 
 	fmt.Println(response)
 }
